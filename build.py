6ce26652bdd25f9fd380b9817f46cb86bb8fd9f3
mikityuk <konstantin.mikityuk@psi.ch>
2022-04-03 14:13:51 +0200

    first content

---
+++ build.py
@@ -0,0 +1,21 @@
+import os
+
+#read template
+f = open('template', mode = 'r')
+t0 = f.read()
+f.close()
+
+#split
+t1 = t0.split('INSERTHERE')
+
+for f_name in os.listdir('.'):
+    if f_name.endswith('.i'):
+        fname = f_name[0:f_name.find('.')]
+        #read fname.i
+        f = open(fname+'.i', mode = 'r')
+        i0 = f.read()
+        f.close()
+        #write fname.html
+        f = open(fname+'.html', mode = 'w')
+        f.write(t1[0] + i0 + t1[1])
+        f.close()
